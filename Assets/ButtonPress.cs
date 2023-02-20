3d6f11bb581cb7a2b60b0829e55be1501107dd70
#GARG ASTHA# <GARG0002@student.main.ntu.edu.sg>
2023-02-20 17:26:46 +0800

    updates

--- Assets/ButtonPress.cs
+++ Assets/ButtonPress.cs
@@ -1,29 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class ButtonPress : MonoBehaviour
 {
     private bool isPressed=false;
+
+    public float buttonDepth = 0.1f;
+    public float pressDuration = 0.5f;
+
+    private Vector3 initialPosition;
+    private float pressStartTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.position;
     }
 
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("player"))
+        {
+            isPressed = true;
+            pressStartTime = Time.time;
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        print("in collision");
-        if(!isPressed && other.gameObject.CompareTag("player"))
+        if(isPressed && Time.time - pressStartTime < pressDuration)
         {
-            isPressed=true;
+            transform.position = initialPosition - new Vector3(0, buttonDepth, 0);
             print("button pressed");
-            transform.position = new Vector3(transform.position.x, transform.position.y-0.5f, transform.position.z);
+        }
+
+        else
+        {
+            transform.position = initialPosition;
+            isPressed = false;
         }
     }
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     print("in collision");
+    //     if(!isPressed && other.gameObject.CompareTag("player"))
+    //     {
+    //         isPressed=true;
+    //         print("button pressed");
+    //         transform.position = new Vector3(transform.position.x, transform.position.y-0.5f, transform.position.z);
+    //     }
+    // }
+
+
 }
