83b2b8cf65d3573263fc7f1f047dd7c0addf2dd4
#GARG ASTHA# <GARG0002@student.main.ntu.edu.sg>
2023-02-22 17:17:32 +0800

    ui

---
+++ Assets/SceneHandler.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public SteamVR_LaserPointer laserPointer;
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+SceneManager.LoadScene("TutorialScene");
+            Debug.Log("Cube was clicked");
+        } else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was clicked");
+        }
+        else if (e.target.name == "Tutorial")
+        {
+            Debug.Log("Button was clicked");
+            SceneManager.LoadScene("TutorialScene");
+        }
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was entered");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was entered");
+        }
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was exited");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was exited");
+        }
+    }
+}
---
+++ Assets/UIscript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIscript : MonoBehaviour
+{
+    public GameObject UI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
